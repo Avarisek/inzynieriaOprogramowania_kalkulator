60b72124b43250d41e4c788c515966fe42d0a9fe
Piotr Piszora <piotr.piszora@gmail.com>
2022-03-19 19:25:24 +0100

    poprawiono roznice w typach danych dodawania

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def hello(name):
 
 def odejmij(a,b):
 	wynik=float(a)+float(b)
-	return a-b
+	return wynik
 
 print ("Hello World")
 
@@ -13,8 +13,8 @@ def dodaj(a,b):
 	wynik=float(a)+float(b)
 	return wynik
 
-pierwsza=int(input())
-druga=int(input())
+pierwsza=input()
+druga=input()
 
 print(dodaj(pierwsza,druga))
 
